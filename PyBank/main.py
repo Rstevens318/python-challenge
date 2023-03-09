ee8275f537b29d9fd4f2fd21a6ebb47042952de4
Robert  Stevens <Rstevens318@gmail.com>
2023-03-09 18:21:26 -0500

    edit of final before print

--- PyBank/main.py
+++ PyBank/main.py
@@ -28,13 +28,13 @@ with open(budget_data, encoding= 'utf-8') as budget_file:
         rowcount += 1
 #Count Sum of Profits/Losses
         total += int(row[1])
-#Get Average change in Profits/Losses over the period (Month) and take an average
+#Assign current value before looping through rows
         currentValue = int(row[1])
-
+#Start loop
         if rowcount == 1:
-
+#Set last Value at start of loop
             lastValue = currentValue
-
+#Set value change per month and update lists
         else:
         
             valueChanges = currentValue - lastValue
@@ -44,17 +44,19 @@ with open(budget_data, encoding= 'utf-8') as budget_file:
             monthlyValueChange.append(valueChanges)
 
             lastValue = currentValue
-
+#Get Average of profit/losses
     averageValue = sum(monthlyValueChange)/len(monthOfChange)
-
+#Get Min and Max of monthly profit/loss
     greatestIncrease = max(monthlyValueChange)
     greatestDecrease = min(monthlyValueChange)
+#Set the Min and Max in the list
     greatestMonthIncrease = monthlyValueChange.index(greatestIncrease)
     greatestMonthDecrease = monthlyValueChange.index(greatestDecrease)
+#Get the month associated with the min and max from the list
     highestMonth = monthOfChange[greatestMonthIncrease]
     lowestMonth = monthOfChange[greatestMonthDecrease]
 
-
+#Print to terminal to double check work
 print('Total Months:', rowcount)
 print('Total:', '$',total)
 print('%.2f' % averageValue)
@@ -62,3 +64,6 @@ print(greatestIncrease)
 print(greatestDecrease)
 print(highestMonth)
 print(lowestMonth)
+
+
+#Export data to .txt file
