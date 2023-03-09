e66ce8b0e2394f745f92cb921de15ea3237ce4f8
Robert  Stevens <Rstevens318@gmail.com>
2023-03-09 18:15:02 -0500

    final before print

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,31 +6,59 @@ import csv
 #Define path to csv file
 budget_data = os.path.join("..","PyBank", "Resources", "budget_data.csv")
 
+#Set Variables
+rowcount = 0
+total = 0
+currentValue = 0
+lastValue = 0
+valueChanges = 0
+monthlyValueChange = []
+monthOfChange = []
+
 #Open csv file
 with open(budget_data, encoding= 'utf-8') as budget_file:
 
 #Define reader and skip headers
     csvreader = csv.reader(budget_file, delimiter=',')
     csvheader = next(csvreader)
-   
-#Set Variables
-    rowcount = 0
-    total = 0
-    averageChange = 0
-    lastValue = 0
-    lastValueChange = []
-    monthOfChange = []
 
 #Start Counter 
     for row in csvreader:
 #Count Total Rows
-        rowcount+= 1
+        rowcount += 1
 #Count Sum of Profits/Losses
-        total+= int(row[1])
-#Get Average change in Profits/Losses over the period and take an average
+        total += int(row[1])
+#Get Average change in Profits/Losses over the period (Month) and take an average
+        currentValue = int(row[1])
+
+        if rowcount == 1:
+
+            lastValue = currentValue
+
+        else:
         
+            valueChanges = currentValue - lastValue
+
+            monthOfChange.append(row[0])
+
+            monthlyValueChange.append(valueChanges)
+
+            lastValue = currentValue
+
+    averageValue = sum(monthlyValueChange)/len(monthOfChange)
+
+    greatestIncrease = max(monthlyValueChange)
+    greatestDecrease = min(monthlyValueChange)
+    greatestMonthIncrease = monthlyValueChange.index(greatestIncrease)
+    greatestMonthDecrease = monthlyValueChange.index(greatestDecrease)
+    highestMonth = monthOfChange[greatestMonthIncrease]
+    lowestMonth = monthOfChange[greatestMonthDecrease]
 
 
 print('Total Months:', rowcount)
 print('Total:', '$',total)
-print(monthOfChange)
+print('%.2f' % averageValue)
+print(greatestIncrease)
+print(greatestDecrease)
+print(highestMonth)
+print(lowestMonth)
